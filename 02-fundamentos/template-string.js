b59a398f34eea5509e26f76c5086c907689391b3
Mariangel Yajure <mariangel.yajure@gmail.com>
2022-04-03 14:53:23 -0430

    Templates strings

---
+++ 02-fundamentos/template-string.js
@@ -0,0 +1,15 @@
+const nombre = 'Deadpool';
+const real = 'Wade Winston';
+
+const normal = nombre + ' ' + real;
+
+const template = `${nombre} ${real}` //Backticks o como se escriba xd
+
+console.log(template); 
+
+const html = `
+    <h1>Hola</h1>
+    <p>Mundo</p>
+`; //Saltos de línea
+
+console.log(html);
