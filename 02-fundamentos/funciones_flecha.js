8cc252b4d0c3ac815392e0b6b4ee4c4e77b45e25
Mariangel Yajure <mariangel.yajure@gmail.com>
2022-04-08 10:03:22 -0430

    Funciones de flecha

---
+++ 02-fundamentos/funciones_flecha.js
@@ -0,0 +1,15 @@
+// function sumar(a, b){
+// 	return a + b;
+// }
+
+// const sumar = (a, b) => {
+// 	return a + b;
+// }
+
+const sumar = (a, b) => a + b; //Es lo mismo del return, sólo si es una sola línea
+
+const saludar = () => 'Hola mundo!';
+
+
+console.log(sumar(10, 5));
+console.log(saludar());
