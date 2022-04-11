60a7f235997a97e60355d877b19252a488833da9
Mariangel Yajure <mariangel.yajure@gmail.com>
2022-04-11 15:23:42 -0430

    Importando archivos al proyecto y transformando una función a asíncrona

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,22 +1,9 @@
-const fs = require('fs'); //Require de file system, para más información buscar en la documentación de nodejs
+const {crearArchivo} = require('./helpers/multiplicar.js');
 
 console.clear();
 
-console.log('===========================');
-console.log('	Tabla ');
-console.log('===========================');
+const base = 1;
 
-const base = 2;
-let salida = '';
-
-for(let i = 1; i <= 10; i++){
-	salida += `${base} x ${i} = ${base*i}\n`;
-}
-
-fs.writeFile(`tabla-${base}.txt`, salida, (err) => { //Tiene un callback, una función que se ejecutará cuando todo el procedimiento termine.
-
-	if(err) throw err;
-
-	console.log(`tabla-${base}.txt creada!`);
-})
-
+crearArchivo(base)
+	.then(nombreArchivo => console.log(nombreArchivo, 'creado'))
+	.catch(err => console.log(err));
