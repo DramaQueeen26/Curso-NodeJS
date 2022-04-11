5b502da7cccca5f70700d34afabf8df1a17847a8
Mariangel Yajure <mariangel.yajure@gmail.com>
2022-04-11 14:12:12 -0430

    Bases de node - Inicio del proyecto

---
+++ 03-bases-node/app.js
@@ -0,0 +1,14 @@
+//Imprimir tabla del 5 por consola
+
+console.clear();
+
+const base = 5;
+
+console.log('===========================');
+console.log('	Tabla del 5');
+console.log('===========================');
+
+
+for(let i = 1; i <= 10; i++){
+	console.log(`${base} x ${i} = ${base*i}`);
+}
