16ab7d7abc00f10c096256074262848ed2266b35
Mariangel Yajure <mariangel.yajure@gmail.com>
2022-04-11 14:33:45 -0430

    Grabando archivo txt con file system en el directorio

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,14 +1,22 @@
-//Imprimir tabla del 5 por consola
+const fs = require('fs'); //Require de file system, para más información buscar en la documentación de nodejs
 
 console.clear();
 
-const base = 5;
-
 console.log('===========================');
-console.log('	Tabla del 5');
+console.log('	Tabla ');
 console.log('===========================');
 
+const base = 2;
+let salida = '';
 
 for(let i = 1; i <= 10; i++){
-	console.log(`${base} x ${i} = ${base*i}`);
+	salida += `${base} x ${i} = ${base*i}\n`;
 }
+
+fs.writeFile(`tabla-${base}.txt`, salida, (err) => { //Tiene un callback, una función que se ejecutará cuando todo el procedimiento termine.
+
+	if(err) throw err;
+
+	console.log(`tabla-${base}.txt creada!`);
+})
+
